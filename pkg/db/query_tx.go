5374e5b972a4d974389ef00895ffd58fe7981beb
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2024-11-24 17:27:31 +0100

    feat: Added definition of types taken from user-service

---
+++ pkg/db/query_tx.go
@@ -0,0 +1,54 @@
+package db
+
+import (
+	"context"
+
+	"github.com/KnoblauchPilze/backend-toolkit/pkg/db/pgx"
+	"github.com/KnoblauchPilze/backend-toolkit/pkg/errors"
+	jpgx "github.com/jackc/pgx/v5"
+)
+
+func QueryOneTx[T any](ctx context.Context, tx Transaction, sql string, arguments ...any) (T, error) {
+	var out T
+
+	txImpl, ok := tx.(*transactionImpl)
+	if !ok {
+		return out, errors.NewCode(UnsupportedOperation)
+	}
+	rows, err := txImpl.query(ctx, sql, arguments...)
+	if err != nil {
+		return out, pgx.AnalyzeAndWrapPgError(err)
+	}
+
+	out, err = jpgx.CollectExactlyOneRow(rows, getCollectorForType[T]())
+	if err != nil {
+		if err == jpgx.ErrNoRows {
+			return out, errors.WrapCode(err, NoMatchingRows)
+		} else if err == jpgx.ErrTooManyRows {
+			return out, errors.WrapCode(err, TooManyMatchingRows)
+		}
+		return out, err
+	}
+
+	return out, nil
+}
+
+func QueryAllTx[T any](ctx context.Context, tx Transaction, sql string, arguments ...any) ([]T, error) {
+	var out []T
+
+	txImpl, ok := tx.(*transactionImpl)
+	if !ok {
+		return out, errors.NewCode(UnsupportedOperation)
+	}
+	rows, err := txImpl.query(ctx, sql, arguments...)
+	if err != nil {
+		return out, pgx.AnalyzeAndWrapPgError(err)
+	}
+
+	out, err = jpgx.CollectRows(rows, getCollectorForType[T]())
+	if err != nil {
+		return out, errors.WrapCode(err, UnsupportedOperation)
+	}
+
+	return out, nil
+}
---
+++ pkg/middleware/errors.go
@@ -0,0 +1,7 @@
+package middleware
+
+import "github.com/KnoblauchPilze/backend-toolkit/pkg/errors"
+
+const (
+	UncaughtPanic errors.ErrorCode = 400
+)
