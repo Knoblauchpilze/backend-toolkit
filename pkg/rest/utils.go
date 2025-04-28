9625356f871a1c069aaebc7c3a18b3b007a55766
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2025-04-28 20:59:04 +0200

    feat: Added MarshalToEmptySlice and FetchIdFromQueryParam to rest/utils

--- pkg/rest/utils.go
+++ pkg/rest/utils.go
@@ -1,9 +1,13 @@
 package rest
 
 import (
+	"encoding/json"
 	"fmt"
 	"regexp"
 	"strings"
+
+	"github.com/google/uuid"
+	"github.com/labstack/echo/v4"
 )
 
 var multiSlashRegex = regexp.MustCompile("[/]+")
@@ -23,4 +27,24 @@ func sanitizePath(route string) string {
 func ConcatenateEndpoints(basePath string, path string) string {
 	concatenated := fmt.Sprintf("/%s/%s", basePath, path)
 	return sanitizePath(concatenated)
+}
+
+func MarshalNilToEmptySlice[T any](in []T) ([]byte, error) {
+	toMarshal := make([]T, 0)
+	if in != nil {
+		toMarshal = in
+	}
+
+	return json.Marshal(toMarshal)
+}
+
+func FetchIdFromQueryParam(key string, c echo.Context) (exists bool, id uuid.UUID, err error) {
+	maybeId := c.QueryParam(key)
+	exists = (maybeId != "")
+	if maybeId == "" {
+		return exists, uuid.UUID{}, nil
+	}
+
+	id, err = uuid.Parse(maybeId)
+	return exists, id, err
 }
