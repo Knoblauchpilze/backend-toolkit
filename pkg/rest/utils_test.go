8c84d71955cbd27b528fe2c9fa6a7ff5fac7dc31
KnoblauchPilze <prolific-programmer@tuta.com>
2025-04-28 20:59:04 +0200

    feat: Added MarshalToEmptySlice and FetchIdFromQueryParam to rest/utils

--- pkg/rest/utils_test.go
+++ pkg/rest/utils_test.go
@@ -1,10 +1,16 @@
 package rest
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
+	"github.com/google/uuid"
+	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
 )
+
+var sampleUuid = uuid.MustParse("08ce96a3-3430-48a8-a3b2-b1c987a207ca")
 
 func TestUnit_SanitizePath(t *testing.T) {
 	type testCase struct {
@@ -68,4 +74,94 @@ func TestUnit_ConcatenateEndpoints(t *testing.T) {
 			assert.Equal(t, testCase.expected, actual)
 		})
 	}
+}
+
+func TestUnit_MarshalNilToEmptySlice_WhenNil_ExpectMarshalToEmptySlice(t *testing.T) {
+	assert := assert.New(t)
+
+	var in []int
+
+	actual, err := MarshalNilToEmptySlice(in)
+
+	assert.Nil(err)
+	assert.Equal("[]", string(actual))
+}
+
+func TestUnit_MarshalNilToEmptySlice_WhenNotNil_ExpectMarshalCorrectData(t *testing.T) {
+	assert := assert.New(t)
+
+	in := []int{1, 2}
+
+	actual, err := MarshalNilToEmptySlice(in)
+
+	assert.Nil(err)
+	assert.Equal("[1,2]", string(actual))
+}
+
+var defaultKey = "my-key"
+
+func TestUnit_FetchIdFromQueryParam_whenNoId_expectNotExistAndNoError(t *testing.T) {
+	assert := assert.New(t)
+
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	ctx, _ := generateTestEchoContextFromRequest(req)
+
+	exists, _, err := FetchIdFromQueryParam(defaultKey, ctx)
+	assert.False(exists)
+	assert.Nil(err)
+}
+
+func TestUnit_FetchIdFromQueryParam_whenIdSetForOtherKey_expectNotExistAndNoError(t *testing.T) {
+	assert := assert.New(t)
+
+	req := generateRequestWithQueryParams("not-the-default-key", sampleUuid.String())
+	ctx, _ := generateTestEchoContextFromRequest(req)
+
+	exists, _, err := FetchIdFromQueryParam(defaultKey, ctx)
+	assert.False(exists)
+	assert.Nil(err)
+}
+
+func TestUnit_FetchIdFromQueryParam_whenIdSyntaxIsWrong_expectExistAndError(t *testing.T) {
+	assert := assert.New(t)
+
+	req := generateRequestWithQueryParams(defaultKey, "not-a-uuid")
+	ctx, _ := generateTestEchoContextFromRequest(req)
+
+	exists, _, err := FetchIdFromQueryParam(defaultKey, ctx)
+	assert.True(exists)
+	assert.Equal("invalid UUID length: 10", err.Error())
+}
+
+func TestUnit_FetchIdFromQueryParam_whenIdIsSet_expectExistCorrectIdAndNoError(t *testing.T) {
+	assert := assert.New(t)
+
+	req := generateRequestWithQueryParams(defaultKey, sampleUuid.String())
+	ctx, _ := generateTestEchoContextFromRequest(req)
+
+	exists, actual, err := FetchIdFromQueryParam(defaultKey, ctx)
+	assert.True(exists)
+	assert.Equal(sampleUuid, actual)
+	assert.Nil(err)
+}
+
+func generateRequestWithQueryParams(key string, value string) *http.Request {
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+
+	q := req.URL.Query()
+	q.Add(key, value)
+
+	req.URL.RawQuery = q.Encode()
+
+	return req
+}
+
+func generateTestEchoContextFromRequest(
+	req *http.Request,
+) (echo.Context, *httptest.ResponseRecorder) {
+	e := echo.New()
+	rw := httptest.NewRecorder()
+
+	ctx := e.NewContext(req, rw)
+	return ctx, rw
 }
