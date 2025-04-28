030419b2f8c13cccb61eeb9920c0417cf780151b
KnoblauchPilze <prolific-programmer@tuta.com>
2025-04-28 21:02:01 +0200

    feat: Added SafeRunSync to process package

---
+++ pkg/process/safe_run_sync.go
@@ -0,0 +1,29 @@
+package process
+
+import (
+	"fmt"
+
+	"github.com/Knoblauchpilze/backend-toolkit/pkg/errors"
+)
+
+type Process func()
+
+func SafeRunSync(proc Process) error {
+	var err error
+
+	func() {
+		defer func() {
+			if recovered := recover(); recovered != nil {
+				if asErr, ok := recovered.(error); ok {
+					err = asErr
+				} else {
+					err = errors.New(fmt.Sprintf("%v", recovered))
+				}
+			}
+		}()
+
+		proc()
+	}()
+
+	return err
+}
