38a9f4a9fa9c32c8171b86e6b9b96e9d3afdaaef
KnoblauchPilze <prolific-programmer@tuta.com>
2025-04-29 21:46:06 +0200

    chore: Removed debug logs

--- pkg/process/signal_handler.go
+++ pkg/process/signal_handler.go
@@ -2,7 +2,6 @@ package process
 
 import (
 	"context"
-	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
@@ -30,11 +29,8 @@ func AsyncStartWithSignalHandler(
 	done := make(chan error, 1)
 
 	go func() {
-		fmt.Printf("started async process\n")
 		err := SafeRunSync(process.Run)
-		fmt.Printf("process returned with err: %v\n", err)
 		done <- err
-		fmt.Printf("async loop finished\n")
 	}()
 
 	waitFunc := func() error {
@@ -44,14 +40,10 @@ func AsyncStartWithSignalHandler(
 
 		select {
 		case <-sCtx.Done():
-			fmt.Printf("interrupting async process\n")
 			err = process.Interrupt()
-			fmt.Printf("interrupt returned: %v\n", err)
 		case err = <-done:
-			fmt.Printf("async process finished: %v\n", err)
 		}
 
-		fmt.Printf("waiting finished, err: %v\n", err)
 		return err
 	}
 
