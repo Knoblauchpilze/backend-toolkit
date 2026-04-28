9cb540da28bdf3e3d919eb697ccb60ac0710724a
Knoblauchpilze <prolific-programmer@tuta.com>
2026-04-28 07:55:36 +0200

    chore: Added example value for request id

--- pkg/rest/response_envelope.go
+++ pkg/rest/response_envelope.go
@@ -1,7 +1,7 @@
 package rest
 
 type ResponseEnvelope[T any] struct {
-	RequestId string `json:"requestId"`
+	RequestId string `json:"requestId" format:"uuid" example:"669cd40f-ea15-40a8-ab03-81e704a3ecf9"`
 	Status    string `json:"status"`
 	Details   T      `json:"details"`
 }
