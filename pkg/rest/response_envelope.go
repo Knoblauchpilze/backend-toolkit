3cd37a99b7b22a26c60ae082388da061a9fb8e2c
Knoblauchpilze <prolific-programmer@tuta.com>
2026-05-01 13:21:30 +0200

    feat: Added required annotations in response envelope fields

--- pkg/rest/response_envelope.go
+++ pkg/rest/response_envelope.go
@@ -1,7 +1,7 @@
 package rest
 
 type ResponseEnvelope[T any] struct {
-	RequestId string `json:"requestId" format:"uuid" example:"669cd40f-ea15-40a8-ab03-81e704a3ecf9"`
-	Status    Status `json:"status" example:"SUCCESS" description:"Request status"`
-	Details   T      `json:"details"`
+	RequestId string `json:"requestId" binding:"required" format:"uuid" example:"669cd40f-ea15-40a8-ab03-81e704a3ecf9"`
+	Status    Status `json:"status" binding:"required" example:"SUCCESS" description:"Request status"`
+	Details   T      `json:"details" binding:"required"`
 }
