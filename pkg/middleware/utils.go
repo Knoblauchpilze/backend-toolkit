a4b4605b321c0287296489b9b360c5e396d3df80
Knoblauchpilze <prolific-programmer@tuta.com>
2026-06-13 17:06:47 +0200

    chore: Removed obsolete code

--- pkg/middleware/utils.go
+++ pkg/middleware/utils.go
@@ -5,22 +5,8 @@ import (
 	"net/http"
 
 	"github.com/Knoblauchpilze/backend-toolkit/pkg/errors"
-	"github.com/Knoblauchpilze/backend-toolkit/pkg/logger"
 	"github.com/labstack/echo/v5"
 )
-
-func formatHttpStatusCode(status int) string {
-	switch {
-	case status >= 500:
-		return logger.FormatWithColor(status, logger.Red)
-	case status >= 400:
-		return logger.FormatWithColor(status, logger.Yellow)
-	case status >= 300:
-		return logger.FormatWithColor(status, logger.Cyan)
-	default:
-		return logger.FormatWithColor(status, logger.Green)
-	}
-}
 
 func wrapToHttpError(err error) error {
 	var httpErr *echo.HTTPError
