8c4c438d8af62c19e7d60874a15ed71b19d0aeb5
Knoblauchpilze <prolific-programmer@tuta.com>
2026-06-14 10:53:48 +0200

    feat: Adapted query tests

--- pkg/db/query_test.go
+++ pkg/db/query_test.go
@@ -5,7 +5,6 @@ import (
 	"testing"
 	"time"
 
-	"github.com/Knoblauchpilze/backend-toolkit/pkg/errors"
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -17,213 +16,216 @@ type dummyConnection struct {
 
 const sampleSqlQuery = "SELECT name FROM my_table"
 
-func TestUnit_QueryOne_UnsupportedConnection(t *testing.T) {
-	_, err := QueryOne[int](context.Background(), &dummyConnection{}, sampleSqlQuery)
+func TestUnit_QueryOne(t *testing.T) {
+	t.Run("returns error when connection is not supported", func(t *testing.T) {
+		_, err := QueryOne[int](context.Background(), &dummyConnection{}, sampleSqlQuery)
 
-	assert.NotNil(t, err)
-	assert.Equal(t, ErrUnsupportedOperation, err, "Actual err: %v", err)
+		assert.ErrorIs(t, ErrUnsupportedOperation, err, "Actual err: %v", err)
+	})
+
+	t.Run("returns error when connection is closed", func(t *testing.T) {
+		conn := newTestConnection(t)
+		conn.Close(context.Background())
+
+		_, err := QueryOne[int](context.Background(), conn, sampleSqlQuery)
+
+		assert.ErrorIs(t, ErrNotConnected, err, "Actual err: %v", err)
+	})
+
+	t.Run("returns error when SQL query is invalid", func(t *testing.T) {
+		conn := newTestConnection(t)
+
+		sqlQuery := "SELECT name FROM my_tables"
+		_, err := QueryOne[string](context.Background(), conn, sqlQuery)
+
+		actual, ok := AsDatabaseError(err)
+		require.True(t, ok)
+		assert.Equal(t, ErrGenericSqlError, actual.Code, "Actual err: %v", err)
+		assert.NotNil(t, actual.Cause)
+	})
+
+	t.Run("returns error when no row matches", func(t *testing.T) {
+		conn := newTestConnection(t)
+
+		sqlQuery := "SELECT id, name FROM my_table WHERE name = $1"
+		_, err := QueryOne[element](context.Background(), conn, sqlQuery, "does-not-exist")
+
+		assert.ErrorIs(t, ErrNoMatchingRows, err, "Actual err: %v", err)
+
+	})
+
+	t.Run("returns error when more than one row matches", func(t *testing.T) {
+		conn := newTestConnection(t)
+		v1 := insertTestData(t, conn)
+		v2 := insertTestData(t, conn)
+
+		sqlQuery := "SELECT id, name FROM my_table WHERE id IN ($1, $2)"
+		_, err := QueryOne[element](context.Background(), conn, sqlQuery, v1.Id, v2.Id)
+
+		assert.ErrorIs(t, ErrTooManyMatchingRows, err, "Actual err: %v", err)
+	})
+
+	t.Run("returns error when SQL constraint is violated", func(t *testing.T) {
+		conn := newTestConnection(t)
+		data := insertTestData(t, conn)
+
+		duplicate := element{
+			Id:   uuid.New(),
+			Name: data.Name,
+		}
+
+		sqlQuery := "INSERT INTO my_table (id, name) VALUES($1, $2)"
+		_, err := QueryOne[element](context.Background(), conn, sqlQuery, duplicate.Id, duplicate.Name)
+
+		actual, ok := AsDatabaseError(err)
+		require.True(t, ok)
+		expected := &DatabaseError{
+			Code:       ErrUniqueConstraintViolation,
+			Message:    "duplicate key value violates unique constraint \"my_table_name_key\"",
+			SqlCode:    "23505",
+			Schema:     "test_db_schema",
+			Table:      "my_table",
+			Column:     "",
+			Constraint: "my_table_name_key",
+			Cause:      actual.Cause,
+		}
+		assert.Equal(t, expected, actual, "Actual err: %v", err)
+	})
+
+	t.Run("successfully maps to struct", func(t *testing.T) {
+		conn := newTestConnection(t)
+		expected := insertTestData(t, conn)
+
+		sqlQuery := "SELECT id, name FROM my_table WHERE name = $1"
+		actual, err := QueryOne[element](context.Background(), conn, sqlQuery, expected.Name)
+		require.NoError(t, err, "Actual err: %v", err)
+
+		assert.Equal(t, expected, actual)
+	})
+
+	t.Run("successfully maps to string", func(t *testing.T) {
+		conn := newTestConnection(t)
+		expected := insertTestData(t, conn)
+
+		sqlQuery := "SELECT name FROM my_table WHERE id = $1"
+		actual, err := QueryOne[string](context.Background(), conn, sqlQuery, expected.Id)
+		require.NoError(t, err, "Actual err: %v", err)
+
+		assert.Equal(t, expected.Name, actual)
+	})
+
+	t.Run("successfully maps to UUID", func(t *testing.T) {
+		conn := newTestConnection(t)
+		expected := insertTestData(t, conn)
+
+		sqlQuery := "SELECT id FROM my_table WHERE name = $1"
+		actual, err := QueryOne[uuid.UUID](context.Background(), conn, sqlQuery, expected.Name)
+		require.NoError(t, err, "Actual err: %v", err)
+
+		assert.Equal(t, expected.Id, actual)
+	})
+
+	t.Run("successfully maps to time", func(t *testing.T) {
+		conn := newTestConnection(t)
+		beforeInsert := time.Now()
+		expected := insertTestData(t, conn)
+
+		sqlQuery := "SELECT updated_at FROM my_table WHERE name = $1"
+		actual, err := QueryOne[time.Time](context.Background(), conn, sqlQuery, expected.Name)
+		require.NoError(t, err, "Actual err: %v", err)
+
+		assert.True(t, beforeInsert.Before(actual))
+	})
 }
 
-func TestIT_QueryOne_WhenClosed_ExpectFailure(t *testing.T) {
-	conn := newTestConnection(t)
-	conn.Close(context.Background())
+func TestUnit_QueryAll(t *testing.T) {
+	t.Run("returns error when connection is not supported", func(t *testing.T) {
+		_, err := QueryAll[int](context.Background(), &dummyConnection{}, sampleSqlQuery)
 
-	_, err := QueryOne[int](context.Background(), conn, sampleSqlQuery)
+		assert.ErrorIs(t, ErrUnsupportedOperation, err, "Actual err: %v", err)
+	})
 
-	assert.NotNil(t, err)
-	assert.Equal(t, ErrNotConnected, err, "Actual err: %v", err)
-}
+	t.Run("returns error when connection is closed", func(t *testing.T) {
+		conn := newTestConnection(t)
+		conn.Close(context.Background())
 
-func TestIT_QueryOne_WhenConnectionFails_ExpectFailure(t *testing.T) {
-	conn := newTestConnection(t)
+		_, err := QueryAll[int](context.Background(), conn, sampleSqlQuery)
 
-	sqlQuery := "SELECT name FROM my_tables"
-	_, err := QueryOne[string](context.Background(), conn, sqlQuery)
+		assert.ErrorIs(t, ErrNotConnected, err, "Actual err: %v", err)
+	})
 
-	assert.NotNil(t, err)
-	actual, ok := errors.AsErrorWithCode(err)
-	require.True(t, ok)
-	assert.Equal(t, ErrGenericSqlError, actual.Code, "Actual err: %v", err)
+	t.Run("returns error when SQL query is invalid", func(t *testing.T) {
+		conn := newTestConnection(t)
 
-	cause := errors.Unwrap(err)
-	assert.NotNil(t, cause)
-}
+		sqlQuery := "SELECT name FROM my_tables"
+		_, err := QueryAll[string](context.Background(), conn, sqlQuery)
 
-func TestIT_QueryOne_WhenNoData_ExpectFailure(t *testing.T) {
-	conn := newTestConnection(t)
+		actual, ok := AsDatabaseError(err)
+		require.True(t, ok)
+		assert.Equal(t, ErrGenericSqlError, actual.Code, "Actual err: %v", err)
+		assert.NotNil(t, actual.Cause)
+	})
 
-	sqlQuery := "SELECT id, name FROM my_table WHERE name = $1"
-	_, err := QueryOne[element](context.Background(), conn, sqlQuery, "does-not-exist")
+	t.Run("successfully fetches no rows", func(t *testing.T) {
+		conn := newTestConnection(t)
 
-	assert.NotNil(t, err)
-	assert.Equal(t, ErrNoMatchingRows, err, "Actual err: %v", err)
-}
+		sqlQuery := "SELECT id, name FROM my_table WHERE name = $1"
+		out, err := QueryAll[element](context.Background(), conn, sqlQuery, "does-not-exist")
+		require.NoError(t, err, "Actual err: %v", err)
 
-func TestIT_QueryOne_WhenTooManyRows_ExpectFailure(t *testing.T) {
-	conn := newTestConnection(t)
-	v1 := insertTestData(t, conn)
-	v2 := insertTestData(t, conn)
+		assert.Empty(t, out)
+	})
 
-	sqlQuery := "SELECT id, name FROM my_table WHERE id IN ($1, $2)"
-	_, err := QueryOne[element](context.Background(), conn, sqlQuery, v1.Id, v2.Id)
+	t.Run("successfully maps to struct", func(t *testing.T) {
+		conn := newTestConnection(t)
+		v1 := insertTestData(t, conn)
+		v2 := insertTestData(t, conn)
 
-	assert.NotNil(t, err)
-	assert.Equal(t, ErrTooManyMatchingRows, err, "Actual err: %v", err)
-}
+		sqlQuery := `SELECT id, name FROM my_table WHERE id IN ($1, $2)`
+		actual, err := QueryAll[element](context.Background(), conn, sqlQuery, v1.Id, v2.Id)
+		require.NoError(t, err, "Actual err: %v", err)
 
-func TestIT_QueryOne_WhenUniqueConstraintViolation_ExpectFailure(t *testing.T) {
-	conn := newTestConnection(t)
-	data := insertTestData(t, conn)
+		expected := []element{v1, v2}
+		assert.Equal(t, expected, actual)
+	})
 
-	duplicate := element{
-		Id:   uuid.New(),
-		Name: data.Name,
-	}
+	t.Run("successfully maps to string", func(t *testing.T) {
+		conn := newTestConnection(t)
+		v1 := insertTestData(t, conn)
+		v2 := insertTestData(t, conn)
 
-	sqlQuery := "INSERT INTO my_table (id, name) VALUES($1, $2)"
-	_, err := QueryOne[element](context.Background(), conn, sqlQuery, duplicate.Id, duplicate.Name)
+		sqlQuery := `SELECT name FROM my_table WHERE id IN ($1, $2)`
+		actual, err := QueryAll[string](context.Background(), conn, sqlQuery, v1.Id, v2.Id)
+		require.NoError(t, err, "Actual err: %v", err)
 
-	actual, ok := errors.AsErrorWithCode(err)
-	require.True(t, ok)
-	assert.Equal(t, ErrUniqueConstraintViolation, actual.Code, "Actual err: %v", err)
-}
+		expected := []string{v1.Name, v2.Name}
+		assert.Equal(t, expected, actual)
+	})
 
-func TestIT_QueryOne_ToStruct(t *testing.T) {
-	conn := newTestConnection(t)
-	expected := insertTestData(t, conn)
+	t.Run("successfully maps to UUID", func(t *testing.T) {
+		conn := newTestConnection(t)
+		v1 := insertTestData(t, conn)
+		v2 := insertTestData(t, conn)
 
-	sqlQuery := "SELECT id, name FROM my_table WHERE name = $1"
-	actual, err := QueryOne[element](context.Background(), conn, sqlQuery, expected.Name)
+		sqlQuery := `SELECT id FROM my_table WHERE name IN ($1, $2)`
+		actual, err := QueryAll[uuid.UUID](context.Background(), conn, sqlQuery, v1.Name, v2.Name)
+		require.NoError(t, err, "Actual err: %v", err)
 
-	assert.Nil(t, err)
-	assert.Equal(t, expected, actual)
-}
+		expected := []uuid.UUID{v1.Id, v2.Id}
+		assert.Equal(t, expected, actual)
+	})
 
-func TestIT_QueryOne_ToString(t *testing.T) {
-	conn := newTestConnection(t)
-	expected := insertTestData(t, conn)
+	t.Run("successfully maps to time", func(t *testing.T) {
+		conn := newTestConnection(t)
+		beforeInsert := time.Now()
+		v1 := insertTestData(t, conn)
+		v2 := insertTestData(t, conn)
 
-	sqlQuery := "SELECT name FROM my_table WHERE id = $1"
-	actual, err := QueryOne[string](context.Background(), conn, sqlQuery, expected.Id)
+		sqlQuery := "SELECT updated_at FROM my_table WHERE id IN ($1, $2)"
+		actual, err := QueryAll[time.Time](context.Background(), conn, sqlQuery, v1.Id, v2.Id)
+		require.NoError(t, err, "Actual err: %v", err)
 
-	assert.Nil(t, err)
-	assert.Equal(t, expected.Name, actual)
-}
-
-func TestIT_QueryOne_ToUuid(t *testing.T) {
-	conn := newTestConnection(t)
-	expected := insertTestData(t, conn)
-
-	sqlQuery := "SELECT id FROM my_table WHERE name = $1"
-	actual, err := QueryOne[uuid.UUID](context.Background(), conn, sqlQuery, expected.Name)
-
-	assert.Nil(t, err)
-	assert.Equal(t, expected.Id, actual)
-}
-
-func TestIT_QueryOne_ToTime(t *testing.T) {
-	conn := newTestConnection(t)
-	beforeInsert := time.Now()
-	expected := insertTestData(t, conn)
-
-	sqlQuery := "SELECT updated_at FROM my_table WHERE name = $1"
-	actual, err := QueryOne[time.Time](context.Background(), conn, sqlQuery, expected.Name)
-
-	assert.Nil(t, err)
-	assert.True(t, beforeInsert.Before(actual))
-}
-
-func TestIT_QueryAll_UnsupportedConnection(t *testing.T) {
-	_, err := QueryAll[int](context.Background(), &dummyConnection{}, sampleSqlQuery)
-
-	assert.NotNil(t, err)
-	assert.Equal(t, ErrUnsupportedOperation, err, "Actual err: %v", err)
-}
-
-func TestIT_QueryAll_WhenClosed_ExpectFailure(t *testing.T) {
-	conn := newTestConnection(t)
-	conn.Close(context.Background())
-
-	_, err := QueryAll[int](context.Background(), conn, sampleSqlQuery)
-
-	assert.NotNil(t, err)
-	assert.Equal(t, ErrNotConnected, err, "Actual err: %v", err)
-}
-
-func TestIT_QueryAll_WhenConnectionFails_ExpectFailure(t *testing.T) {
-	conn := newTestConnection(t)
-
-	sqlQuery := "SELECT name FROM my_tables"
-	_, err := QueryAll[string](context.Background(), conn, sqlQuery)
-
-	assert.NotNil(t, err)
-	actual, ok := errors.AsErrorWithCode(err)
-	require.True(t, ok)
-	assert.Equal(t, ErrGenericSqlError, actual.Code, "Actual err: %v", err)
-
-	cause := errors.Unwrap(err)
-	assert.NotNil(t, cause)
-}
-
-func TestIT_QueryAll_NoData(t *testing.T) {
-	conn := newTestConnection(t)
-
-	sqlQuery := "SELECT id, name FROM my_table WHERE name = $1"
-	out, err := QueryAll[element](context.Background(), conn, sqlQuery, "does-not-exist")
-
-	assert.Nil(t, err)
-	assert.Empty(t, out)
-}
-
-func TestIT_QueryAll_ToStruct(t *testing.T) {
-	conn := newTestConnection(t)
-	v1 := insertTestData(t, conn)
-	v2 := insertTestData(t, conn)
-
-	sqlQuery := `SELECT id, name FROM my_table WHERE id IN ($1, $2)`
-	actual, err := QueryAll[element](context.Background(), conn, sqlQuery, v1.Id, v2.Id)
-
-	assert.Nil(t, err)
-	expected := []element{v1, v2}
-	assert.Equal(t, expected, actual)
-}
-
-func TestIT_QueryAll_ToString(t *testing.T) {
-	conn := newTestConnection(t)
-	v1 := insertTestData(t, conn)
-	v2 := insertTestData(t, conn)
-
-	sqlQuery := `SELECT name FROM my_table WHERE id IN ($1, $2)`
-	actual, err := QueryAll[string](context.Background(), conn, sqlQuery, v1.Id, v2.Id)
-
-	assert.Nil(t, err)
-	expected := []string{v1.Name, v2.Name}
-	assert.Equal(t, expected, actual)
-}
-
-func TestIT_QueryAll_ToUuid(t *testing.T) {
-	conn := newTestConnection(t)
-	v1 := insertTestData(t, conn)
-	v2 := insertTestData(t, conn)
-
-	sqlQuery := `SELECT id FROM my_table WHERE name IN ($1, $2)`
-	actual, err := QueryAll[uuid.UUID](context.Background(), conn, sqlQuery, v1.Name, v2.Name)
-
-	assert.Nil(t, err)
-	expected := []uuid.UUID{v1.Id, v2.Id}
-	assert.Equal(t, expected, actual)
-}
-
-func TestIT_QueryAll_ToTime(t *testing.T) {
-	conn := newTestConnection(t)
-	beforeInsert := time.Now()
-	v1 := insertTestData(t, conn)
-	v2 := insertTestData(t, conn)
-
-	sqlQuery := "SELECT updated_at FROM my_table WHERE id IN ($1, $2)"
-	actual, err := QueryAll[time.Time](context.Background(), conn, sqlQuery, v1.Id, v2.Id)
-
-	assert.Nil(t, err)
-	assert.True(t, beforeInsert.Before(actual[0]))
-	assert.True(t, beforeInsert.Before(actual[1]))
+		assert.True(t, beforeInsert.Before(actual[0]))
+		assert.True(t, beforeInsert.Before(actual[1]))
+	})
 }
