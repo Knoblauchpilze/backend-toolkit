486dae419f4c464a6afad8c4f78742da82a1dafc
Knoblauchpilze <prolific-programmer@tuta.com>
2026-07-20 21:53:31 +0200

    chore: Used t.Run statements and strenghtened test

--- pkg/middleware/request_tracer_test.go
+++ pkg/middleware/request_tracer_test.go
@@ -8,36 +8,42 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestUnit_RequestTracer_CallsNextMiddleware(t *testing.T) {
-	callable, called, ctx := createCallableTracerHandler()
+func TestUnit_RequestTracer(t *testing.T) {
+	t.Run("calls next middleware", func(t *testing.T) {
+		callable, called, ctx := createCallableTracerHandler()
 
-	err := callable(ctx)
+		err := callable(ctx)
+		require.NoError(t, err, "Actual err: %v", err)
 
-	assert.Nil(t, err)
-	assert.True(t, *called)
-}
+		assert.True(t, *called)
+	})
 
-func TestUnit_RequestTracer_WhenRequestIdNotSet_LeavesLoggerUnchanged(t *testing.T) {
-	_, _, ctx := createCallableTracerHandler()
-	originalLogger := ctx.Logger()
+	t.Run("when request id not set leaves logger unchanged", func(t *testing.T) {
+		_, _, ctx := createCallableTracerHandler()
+		originalLogger := ctx.Logger()
 
-	callable, _, _ := createCallableTracerHandler()
-	err := callable(ctx)
-	require.Nil(t, err)
+		callable, _, _ := createCallableTracerHandler()
+		err := callable(ctx)
+		require.NoError(t, err, "Actual err: %v", err)
 
-	assert.Equal(t, originalLogger, ctx.Logger())
-}
+		assert.Equal(t, originalLogger, ctx.Logger())
+	})
 
-func TestUnit_RequestTracer_WhenRequestIdSet_AddsRequestIdToLogger(t *testing.T) {
-	callable, _, ctx := createCallableTracerHandler()
+	t.Run("when request id set adds request id to logger", func(t *testing.T) {
+		callable, _, _ := createCallableTracerHandler()
+		ctx, out := generateTestEchoContextWithLogger()
+		originalLogger := ctx.Logger()
 
-	ctx.Response().Header().Set(requestIdHeader, "my-request-id")
+		ctx.Response().Header().Set(requestIdHeader, "my-request-id")
 
-	err := callable(ctx)
-	require.Nil(t, err)
+		err := callable(ctx)
+		require.NoError(t, err, "Actual err: %v", err)
 
-	// The logger should have been updated (it's a different logger with the requestId attribute)
-	assert.NotNil(t, ctx.Logger())
+		assert.NotEqual(t, originalLogger, ctx.Logger())
+
+		ctx.Logger().Info("test-message")
+		assert.Contains(t, out.String(), `"requestId":"my-request-id"`)
+	})
 }
 
 func createCallableTracerHandler() (echo.HandlerFunc, *bool, *echo.Context) {
