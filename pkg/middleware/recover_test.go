d969c90ad06f1dcf84e8cd4f349f299c88ad84d6
Knoblauchpilze <prolific-programmer@tuta.com>
2026-08-07 08:08:31 +0200

    refactor: Migrated RequestId middleware to use Gin syntax

--- pkg/middleware/recover_test.go
+++ pkg/middleware/recover_test.go
@@ -1,17 +1,14 @@
 package middleware
 
 import (
-	"bytes"
 	stderrors "errors"
 	"io"
-	"log/slog"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 	"time"
 
 	"github.com/Knoblauchpilze/backend-toolkit/pkg/db"
-	"github.com/Knoblauchpilze/backend-toolkit/pkg/rest"
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -25,7 +22,7 @@ func TestUnit_Recover(t *testing.T) {
 	gin.SetMode(gin.TestMode)
 
 	t.Run("calls next middleware and traps panic", func(t *testing.T) {
-		handler, called := createNewPanicHandler()
+		handler, called := createPanicHandlerWithCalledBoolean()
 
 		r := createTestGinRouterWithHandler(t, handler, Recover())
 
@@ -54,7 +51,7 @@ func TestUnit_Recover(t *testing.T) {
 	})
 
 	t.Run("sets status to internal server error when next panics", func(t *testing.T) {
-		handler, _ := createNewPanicHandler()
+		handler, _ := createPanicHandlerWithCalledBoolean()
 
 		r := createTestGinRouterWithHandler(t, handler, Recover())
 
@@ -80,7 +77,7 @@ func TestUnit_Recover(t *testing.T) {
 	})
 
 	t.Run("returns body with error description when next panics", func(t *testing.T) {
-		handler, _ := createNewPanicHandler()
+		handler, _ := createPanicHandlerWithCalledBoolean()
 
 		r := createTestGinRouterWithHandler(t, handler, Recover())
 
@@ -112,7 +109,7 @@ func TestUnit_Recover(t *testing.T) {
 	})
 
 	t.Run("logs error when logger is available", func(t *testing.T) {
-		handler, _ := createNewPanicHandler()
+		handler, _ := createPanicHandlerWithCalledBoolean()
 
 		r := createTestGinRouterWithHandler(t, handler, Recover())
 
@@ -158,48 +155,4 @@ func TestUnit_Recover(t *testing.T) {
 			actual.Message,
 		)
 	})
-}
-
-func createNewPanicHandler() (NewHandlerFunc, *bool) {
-	var called bool
-	handler := func(c *gin.Context) {
-		called = true
-		panic(errSample)
-	}
-
-	return handler, &called
-}
-
-func createTestGinRouterWithHandler(
-	t *testing.T,
-	handler NewHandlerFunc,
-	middlewares ...NewHandlerFunc,
-) *gin.Engine {
-	t.Helper()
-
-	r := gin.New()
-
-	for _, middleware := range middlewares {
-		r.Use(middleware)
-	}
-
-	r.GET("/", handler)
-
-	return r
-}
-
-func createTestRequestWithLogger(
-	t *testing.T,
-	method string,
-) (*http.Request, *bytes.Buffer) {
-	t.Helper()
-
-	var out bytes.Buffer
-
-	slogLogger := slog.New(slog.NewJSONHandler(&out, &slog.HandlerOptions{Level: slog.LevelDebug}))
-	ctx := rest.WithContextLogger(t.Context(), slogLogger)
-
-	req := httptest.NewRequestWithContext(ctx, method, "http://example.com/", nil)
-
-	return req, &out
 }
--- pkg/middleware/request_id_test.go
+++ pkg/middleware/request_id_test.go
@@ -6,53 +6,66 @@ import (
 	"testing"
 
 	"github.com/Knoblauchpilze/backend-toolkit/pkg/rest"
+	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
 func TestUnit_RequestId(t *testing.T) {
 	t.Run("calls next middleware", func(t *testing.T) {
-		callable, called, ctx := createCallableHandler(t, RequestId)
+		handler, called := createHandlerWithCalledBoolean()
 
-		err := callable(ctx)
-		require.NoError(t, err, "Actual err: %v", err)
+		r := createTestGinRouterWithHandler(t, handler, RequestId())
+
+		req := httptest.NewRequest(http.MethodGet, "http://example.com/", nil)
+		rw := httptest.NewRecorder()
+		r.ServeHTTP(rw, req)
 
 		assert.True(t, *called)
 	})
 
 	t.Run("sets response header and context with same value", func(t *testing.T) {
-		callable, _, ctx := createCallableHandler(t, RequestId)
+		var contextValue string
+		requestIdExists := false
+		handler := func(c *gin.Context) {
+			contextValue, requestIdExists = rest.RequestIdFromContext(c.Request.Context())
 
-		err := callable(ctx)
-		require.NoError(t, err, "Actual err: %v", err)
+			c.Status(http.StatusNoContent)
+		}
 
-		headerValue := ctx.Response().Header().Get(requestIdHeader)
+		r := createTestGinRouterWithHandler(t, handler, RequestId())
+
+		req := httptest.NewRequest(http.MethodGet, "http://example.com/", nil)
+		rw := httptest.NewRecorder()
+		r.ServeHTTP(rw, req)
+
+		headerValue := rw.Header().Get(requestIdHeader)
 		assert.NotEmpty(t, headerValue)
 		assert.Regexp(t, `^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$`, headerValue)
 
-		contextValue, exists := rest.RequestIdFromContext(ctx.Request().Context())
-		assert.True(t, exists)
+		assert.True(t, requestIdExists)
 		assert.Equal(t, headerValue, contextValue)
 	})
 
 	t.Run("does not change request id when already present", func(t *testing.T) {
-		existingRequestId := uuid.NewString()
+		var contextValue string
+		requestIdExists := false
+		handler := func(c *gin.Context) {
+			contextValue, requestIdExists = rest.RequestIdFromContext(c.Request.Context())
 
+			c.Status(http.StatusNoContent)
+		}
+
+		r := createTestGinRouterWithHandler(t, handler, RequestId())
+
+		existingRequestId := uuid.NewString()
 		req := httptest.NewRequest(http.MethodGet, "http://example.com/", nil)
 		req.Header.Set(requestIdHeader, existingRequestId)
+		rw := httptest.NewRecorder()
+		r.ServeHTTP(rw, req)
 
-		ctx, _ := generateTestEchoContextFromRequest(t, req)
-		next, called := createTestEchoHandlerFuncWithCalledBoolean()
-
-		callable := RequestId()(next)
-		err := callable(ctx)
-		require.NoError(t, err, "Actual err: %v", err)
-
-		assert.True(t, *called)
-		contextValue, exists := rest.RequestIdFromContext(ctx.Request().Context())
-		assert.True(t, exists)
+		assert.True(t, requestIdExists)
 		assert.Equal(t, existingRequestId, contextValue)
-		assert.Equal(t, existingRequestId, ctx.Response().Header().Get(requestIdHeader))
+		assert.Equal(t, existingRequestId, rw.Header().Get(requestIdHeader))
 	})
 }
